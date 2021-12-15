ff740450ed1e845126afc56fc41700843b03113d
4532413@qq.com <878781>
2021-12-15 14:51:21 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <tower-crane
+        :width="500"
+        :height="400"
+        :load-ratio="13" :wind="12" :weight="34" :margin="11" :tower-height="100" :rotation="23" :inclination="24"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TowerCrane from './components/TowerCrane.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TowerCrane
   }
 }
 </script>
 
 <style>
+html,body{
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  display: flex;
 }
 </style>
